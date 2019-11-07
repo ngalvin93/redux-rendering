15fdcf03b58a8e8551a974d91d8e057c3be6300c
Alvin Ng <ngalvin93@gmail.com>
2019-11-06 19:11:46 -0600

    nametags complete

--- nameTags/index.js
+++ nameTags/index.js
@@ -6,4 +6,12 @@ subscribe(() => render(getState()));
 dispatch({ type: null }); // Here we're making a call to dispatch() - this triggers the first render.
 
 // Write DOM event listeners here, make them dispatch actions to the Redux store 
-
+const txt = document.getElementById('text')
+document.addEventListener('keydown', e => {
+    if (e.code === 'Enter') {
+        e.preventDefault()
+        dispatch({ type: 'ADD_NAME', name: txt.value})
+        console.log(txt.value)
+        txt.value = ''
+    }
+})
--- nameTags/reducer.js
+++ nameTags/reducer.js
@@ -3,11 +3,17 @@ const initialState = [
     "Stuart"
 ];
 
+const deepCopy = (x) => JSON.parse(JSON.stringify(x))
+
 const reducer = (state = initialState, action) => {
     // Handle actions here - make sure you don't mutate the state!
+    const stateCopy = deepCopy(state)
     const { type } = action;
 
     // Add a Name Tag
-
-    return state;
+    switch (type) {
+        case 'ADD_NAME':
+            stateCopy.push(txt.value)
+    }
+    return stateCopy;
 }
