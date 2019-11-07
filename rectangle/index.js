1da629e8a60e0b8742bde647e7bf262d42406f91
Alvin Ng <ngalvin93@gmail.com>
2019-11-06 21:00:03 -0600

    rectangle complete

--- rectangle/index.js
+++ rectangle/index.js
@@ -6,4 +6,12 @@ subscribe(() => render(getState()));
 dispatch({type: null}); // Here we're making a call to dispatch() - this triggers the first render.
 
 // Write DOM event listeners here, make them dispatch actions to the Redux store 
+const widthBtn = document.getElementById('width')
+const heightBtn = document.getElementById('height')
+const redBtn = document.getElementById('red')
+const blueBtn = document.getElementById('blue')
 
+widthBtn.addEventListener('click', e => dispatch({ type: 'WIDTH' }))
+heightBtn.addEventListener('click', e => dispatch({ type: 'HEIGHT' }))
+redBtn.addEventListener('click', e => dispatch({ type: 'RED' }))
+blueBtn.addEventListener('click', e => dispatch({ type: 'BLUE' }))
--- rectangle/reducer.js
+++ rectangle/reducer.js
@@ -8,11 +8,19 @@ const reducer = (state = initialState, action) => {
     // Handle actions here - make sure you don't mutate the state!
     const { type } = action;
 
-    // ACTION 1 - Increment height by 10
-
-    // ACTION 2 - Increment width by 10
-
-    // ACTION 3 - Change the color
-
+    switch (type) {
+    // ACTION 1 - Increment width by 10
+    case 'WIDTH':
+        return {...state, width: state.width + 10}
+    // ACTION 2 - Increment height by 10
+    case 'HEIGHT':
+        return {...state, height: state.height + 10}
+    // ACTION 3 - Change color to red
+    case 'RED':
+        return {...state, color: '#FF0000'}
+    // ACTION 4 - Change color to blue
+    case 'BLUE':
+        return {...state, color: '#0000FF'}
+    }
     return state;
 }
