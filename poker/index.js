bd88f82f7462138ba22dd099e5f1466fef3744c1
Alvin Ng <ngalvin93@gmail.com>
2019-11-06 20:47:06 -0600

    poker complete

--- poker/index.js
+++ poker/index.js
@@ -6,4 +6,8 @@ subscribe(() => render(getState()));
 dispatch({ type: null }); // Here we're making a call to dispatch() - this triggers the first render.
 
 // Write DOM event listeners here, make them dispatch actions to the Redux store 
+const btn1 = document.getElementById('card1Button')
+const btn2 = document.getElementById('card2Button')
 
+btn1.addEventListener('click', e => dispatch({ type: 'RANDOM_1' }))
+btn2.addEventListener('click', e => dispatch({ type: 'RANDOM_2' }))
--- poker/reducer.js
+++ poker/reducer.js
@@ -9,11 +9,40 @@ const initialState = [
     }
 ];
 
+const firstValues = ['2','3','4','5','6','7','8','9','10','A','J','K','Q']
+const secondValues = ['C','S','D','H']
+
+const findRandomCard1 = () => {
+    return firstValues[Math.floor(Math.random()*firstValues.length)];
+}
+
+const findRandomCard2 = () => {
+    return secondValues[Math.floor(Math.random()*secondValues.length)];
+}
+
 const reducer = (state = initialState, action) => {
     // Handle actions here - make sure you don't mutate the state!
     const { type } = action;
 
     // ACTION - Change either card to a random new one (Try implementing one action that both buttons dispatch)
-    
-    return state;
+    switch (type) {
+        case 'RANDOM_1':
+            return [{
+                value: findRandomCard1(),
+                suit: findRandomCard2()
+            },
+                state[1]
+            ]
+            break
+        case 'RANDOM_2':
+            return [
+                state[0],
+            {
+                value: findRandomCard1(),
+                suit: findRandomCard2()
+            }
+            ]
+        default:
+            return state
+    }
 }
